d3fca72dfcfb03a9a32c2c8757910b0e12cf8594
pigi5 <ford.hash@gmail.com>
2017-11-26 19:41:35 -0600

    saved

--- FFTWaves.h
+++ FFTWaves.h
@@ -10,16 +10,31 @@ CSI 4341: Assignment 2: Camera
 
 #include "GL/glut.h"
 
-struct Point {
+
+struct Complex {
+	GLfloat real, imaginary;
+};
+
+struct Vector2 {
+	GLfloat x, z;
+};
+
+struct Vector3 {
 	GLfloat x, y, z;
 };
 
-struct waveVertex {
-	Point vertex;
-	vec2 normal;
-	Point hTilde0;
-	Point hTilde0star;
-	Point originalPos;
+struct WaveVertex {
+	Vector3 vertex;
+	Vector3 normal;
+	Vector3 hTilde0;
+	Vector3 hTilde0star;
+	Vector3 originalPos;
+};
+
+struct HeightDispNorm {
+	Complex height;
+	Vector2 displacement;
+	Vector3 Normal;
 };
 
 class FFTWaves {
